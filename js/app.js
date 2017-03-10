60f966117796796778037b9e84a478e393596975
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-03-10 11:15:43 -0300

    Otimizando Scroll com debounce do Lodash

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 // Mudar tab ao click
 $('[data-group]').each(function(){
 	var $allTarget = $(this).find('[data-target]'),
@@ -48,14 +64,14 @@ $('section').each(function(){
 			id = $(this).attr('id'),
 			$itemMenu = $('a[href="#' + id + '"]');
 	
-	$(window).scroll(function(){
+	$(window).scroll(debounce(function(){
 		var scrollTop = $(window).scrollTop();
 		if(offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
 			$itemMenu.addClass('active');
 		} else {
 			$itemMenu.removeClass('active');
 		}
-	});
+	}, 200));
 });
 
 // Botão do menu mobile
@@ -64,58 +80,67 @@ $('.mobile-btn').click(function(){
 	$('.mobile-menu').toggleClass('active');
 });
 
-$('.slide > :first').addClass('active');
-
 // Slider
+(function(){
 function slider(sliderName, velocidade) {
 	var sliderClass = '.' + sliderName,
-        activeClass = 'active',
-		rotate = setInterval(rotateSlide, velocidade);
+			activeClass = 'active',
+			rotate = setInterval(rotateSlide, velocidade);
 	
-	$('.slide > :first').addClass(activeClass);
-  
-    $(sliderClass).hover(function(){
-      clearInterval(rotate);
-    }, function(){
-      rotate = setInterval(rotateSlide, velocidade);
-    });
-  
-    function rotateSlide(){
-      var activeSlide = $(sliderClass + ' > .' + activeClass),
-          nextSlide = activeSlide.next();
-      if(nextSlide.length == 0){
-        nextSlide = $(sliderClass + ' > :first');
-      }
-      activeSlide.removeClass(activeClass);
-      nextSlide.addClass(activeClass);
-    }
-} 
+	$(sliderClass + ' > :first').addClass(activeClass);
 
-slider('introducao', 2000);
+	$(sliderClass).hover(function(){
+		clearInterval(rotate);
+	}, function() {
+		rotate = setInterval(rotateSlide, velocidade);
+	});
+	
+	function rotateSlide() {
+		var activeSlide = $(sliderClass + ' > .' + activeClass),
+				nextSlide = activeSlide.next();
 
-// Animação ao Scroll
-var $target = $('[data-anime="scroll"]'),
-    animationClass = 'animate',
-    offset = $(window).height() * 3/4;
-
-function animeScroll(){
-  var documentTop = $(document).scrollTop();
-  
-  $target.each(function(){
-    var itemTop = $(this).offset().top;
-    if (documentTop > itemTop - offset){
-      $(this).addClass(animationClass);
-    } else{
-      $(this).removeClass(animationClass);
-    }
-  });
+		if(nextSlide.length == 0) {
+			nextSlide = $(sliderClass + ' > :first');
+		}
+		activeSlide.removeClass(activeClass);
+		nextSlide.addClass(activeClass);
+	} 
 }
 
-animeScroll();
+slider('introducao', 2000);
+})();
 
-$(document).scroll(function(){
-  animeScroll();
-});
+// Animação ao Scroll
+
+(function(){
+	var $target = $('[data-anime="scroll"]'),
+			animationClass = 'animate',
+			offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
+
+
+
+
+
 
 
 
